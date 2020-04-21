ee8f04306293a2b10e780db090be3e05791d25ed
linnil1 <linnil1.886@gmail.com>
2020-04-21 21:43:47 +0800

    Add helm for novnc

---
+++ bulid_docker.sh
@@ -0,0 +1,24 @@
+# Main program
+echo "BUILD labboxmain"
+docker build labboxmain -t linnil1/labboxmain
+docker run -it --rm -v $PWD/labboxmain:/app -e FLASK_APP=labboxmain linnil1/labboxmain flask initdb
+
+# Control docker to start or stop
+echo "BUILD labboxapi-docker"
+docker build labboxapi_docker -t linnil1/labboxapi-docker
+
+# Control k8s to create or delete
+echo "BUILD labboxapi-k8s"
+docker build labboxapi_k8s -t linnil1/labboxapi-k8s
+
+# VNC
+echo "BUILD VNC"
+cd novnc
+git clone https://github.com/novnc/websockify
+git clone https://github.com/novnc/noVNC.git
+docker run -it --rm -v $PWD/noVNC:/app node:alpine sh -c 'cd /app && npm install . && ./utils/use_require.js --with-app --as commonjs' 
+cp -r noVNC/build ./
+cp build/vnc.html build/index.html
+cat token_plugin.py >> websockify/websockify/token_plugins.py
+docker build . -t linnil1/novnc
+cd ..
